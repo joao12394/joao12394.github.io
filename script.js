a2720f44386673cd96011590833da48dafcaf2b5
joao12394 <joaovictor123456789010qwe@gmail.com>
2024-12-04 08:58:02 -0300

    Update script.js
    Signed-off-by: joao12394 <joaovictor123456789010qwe@gmail.com>

--- script.js
+++ script.js
@@ -6,24 +6,25 @@ window.addEventListener('load', () => {
     }, 3000); // Exibe a tela de carregamento por 3 segundos
 });
 
-// Função para trocar as abas
+// Função para abrir as abas
 function openTab(evt, tabName) {
-    // Ocultar todos os conteúdos das abas
     let i, tabcontent, tabbuttons;
+    
+    // Esconde todos os conteúdos das abas
     tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].classList.remove("active");
     }
 
-    // Remover a classe "active" de todos os botões
+    // Remove a classe "active" de todos os botões
     tabbuttons = document.getElementsByClassName("tab-button");
     for (i = 0; i < tabbuttons.length; i++) {
         tabbuttons[i].classList.remove("active");
     }
 
-    // Mostrar o conteúdo da aba clicada
+    // Mostra o conteúdo da aba selecionada
     document.getElementById(tabName).classList.add("active");
 
-    // Adicionar a classe "active" ao botão clicado
+    // Adiciona a classe "active" ao botão da aba selecionada
     evt.currentTarget.classList.add("active");
 }
