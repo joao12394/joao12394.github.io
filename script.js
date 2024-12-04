429c42382ac0a43a5e0797fe22804b8a7ee56be7
joao12394 <joaovictor123456789010qwe@gmail.com>
2024-12-04 08:48:56 -0300

    Update script.js
    Signed-off-by: joao12394 <joaovictor123456789010qwe@gmail.com>

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ window.addEventListener('load', () => {
         document.querySelector('.main-content').style.opacity = 1;
     }, 3000); // Exibe a tela de carregamento por 3 segundos
 });
+
+// Função para trocar as abas
+function openTab(evt, tabName) {
+    // Ocultar todos os conteúdos das abas
+    let i, tabcontent, tabbuttons;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (
